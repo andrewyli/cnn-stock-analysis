4ac1e30d1c0df05ecf928dcb83d7405c221cf016
Andrew Li <andrewyli@gmail.com>
2017-07-27 16:26:55 -0700

    more design notes

--- parse.py
+++ parse.py
@@ -72,7 +72,7 @@ def build_ticker_data(raw_data):
         label_vector.add((raw_data[i + DESIGN_WIDTH] > raw_data[i + DESIGN_WIDTH - 1]))
     return [design_matrix, label_vector]
 
-def generate_training_data(exchange):
+def generate_raw_data(exchange):
     training_data = []
     if not (exchange in data_exists and data_exists[exchange]):
         get_all_data(exchange)
@@ -93,8 +93,6 @@ def generate_training_data(exchange):
 
 
 
-
-
 # each data point is a day? See "sliding window"
 # what can we classify from each of these?
 # - volatility
@@ -102,7 +100,7 @@ def generate_training_data(exchange):
 # - actual ending value
 # sliding window architecture, over intervals of X for each day... or maybe even just the year.
 # are we even using a CNN? Let's consider a 10Y dataset oh but if there are a lot of stocks it's viable
-# 10 Y = 300 M seconds, if we call each image 3000 seconds we could do something in the 100000s. What if we just used a normal NN over windwos of 300 (299 + 1) each to predict the next value?
+# 10 Y = 300 M seconds, if we call each image 3000 seconds we could do something in the 100000s. What if we just used a normal NN over windows of 300 (299 + 1) each to predict the next value?
 # Need to remember to validate on different sets (tickers)
 # Why second-level granularity? Because you can avoid bullshit like the program running too slow in Python and failing to execute the trade on time
 # does the second at which the price is updated matter at all? Also are we classifying up/down/constant or a numerical predictive value? I feel like having an output layer of 3 is good or at least simpler
@@ -111,3 +109,5 @@ def generate_training_data(exchange):
 # I should have a layer that literally just changes the importance of each node based on its seconds value, just a copy layer
 
 # If my NN has a boolean output...is there a better way to do this? or like, what's a simple way of looking at the problem?
+# - Gotcha: https://stats.stackexchange.com/questions/207049/neural-network-for-binary-classification-use-1-or-2-output-neurons
+# - Same: https://stats.stackexchange.com/questions/233658/softmax-vs-sigmoid-function-in-logistic-classifier
